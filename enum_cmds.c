7469b0046da7a65e7a350e0ef0250959b1ef8c77
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-05 10:05:31 -0800

    Add argument parsing function prototypes
    Complete parse_ls_args

--- enum_cmds.c
+++ enum_cmds.c
@@ -11,27 +11,23 @@ typedef struct s_args_array {
   char **array;
 } args_array;
 
-commands find_command(char *argv) {
-  if (strcmp(argv, "add") == 0) {
+commands find_command(char *arg) {
+  if (strcmp(arg, "add") == 0) {
     return ADD;
-  } 
-  else if (strcmp(argv, "rm") == 0) {
+  } else if (strcmp(arg, "rm") == 0) {
     return RM;
-  }
-  else if (strcmp(argv, "ls") == 0) {
+  } else if (strcmp(arg, "ls") == 0) {
     return LS;
-  }
-  else if (strcmp(argv, "sync") == 0) {
+  } else if (strcmp(arg, "sync") == 0) {
     return SYNC;
-  }
-  else if (strcmp(argv, "quit") == 0) {
+  } else if (strcmp(arg, "quit") == 0) {
     return QUIT;
-  }
-  else {
+  } else {
     return NA;
   }
 }
 
+//Return args_array that is a subarray of the input string array
 args_array *subarray(char **argv, int start_index, int end_index) {
   args_array *subarray = malloc(sizeof(args_array));
   subarray->size = end_index - start_index + 1;
@@ -45,6 +41,47 @@ args_array *subarray(char **argv, int start_index, int end_index) {
   return subarray;
 }
 
+int parse_block_args(args_array *args, commands command, int bid, int nid) {
+  switch(command) {
+    case ADD: {
+
+    }
+  }
+}
+
+int parse_node_args() {
+}
+
+int parse_add_args(args_array *args, commands command, int bid, int nid) {
+  if (args->size < 2) {
+    return 1;
+  } else if (strcmp(args->array[0], "block") == 0 && args->size == 3) {
+    //parse_block_args();
+  } else if (strcmp(args->array[0], "node") == 0 && args->size == 2) {
+    //parse_node_args();
+  } else {
+    return 1;
+  }
+  return 0;
+}
+
+int parse_rm_args() {
+  return 0;
+}
+
+//If command = LS, check subsequent arguments for no arguments or -l option
+int parse_ls_args(args_array *args) {
+  if (args->size == 0) {
+    printf("List all nodes by their nid\n");
+  } else if (strcmp(args->array[0], "-l") == 0) {
+    printf("List all nodes' nid and bid\n");
+  } else {
+    printf("ls arguments not recognized\n");
+    return 1;
+  }
+  return 0;
+}
+
 int main(int argc, char **argv) {
   char *bid = NULL;
   char *nid = NULL;
@@ -55,26 +92,30 @@ int main(int argc, char **argv) {
   }
 
   if (argc > 1) {
-    //printf("if statement entered here\n");
     switch(find_command(argv[1])) {
       case ADD: {
         printf("add command found\n");
+        //parse_add_args();
         break;
       }
       case RM: {
         printf("rm command found\n");
+        //parse_rm_args();
         break;
       }
       case LS: {
         printf("ls command found\n");
+        parse_ls_args(args);
         break;
       }
       case SYNC: {
         printf("sync command found\n");
+        printf("Execute sync routine\n");
         break;
       }
       case QUIT: {
         printf("quit command found\n");
+        printf("Execute quit routine\n");
         break;
       }
       default:
