589f1e087be3dabf578ce1bff890831b27bce823
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-06 01:23:42 -0800

    Incoporate defined messages specified in status.h

--- status.h
+++ status.h
@@ -1,1 +1,10 @@
+//Error messages
+#define NODE_EXISTS "This node already exists\n"
+#define BLOCK_EXISTS "This block already exists\n"
+#define NODE_NOT_FOUND "Node doesn't exist\n"
+#define BLOCK_NOT_FOUND "Block doesn't exist\n"
 #define NO_COMMAND_FOUND "Command not found\n"
+
+//Status messages
+#define COMMAND_SUCCESS "OK"
+
