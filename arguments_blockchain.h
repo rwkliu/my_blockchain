b69a8b0b0ad5e87ef69d81395d6123cb862295c7
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-05 21:55:48 -0800

    Copy function prototypes and typedefs from enum_cmds

--- arguments_blockchain.h
+++ arguments_blockchain.h
@@ -1,5 +1,12 @@
 #define READ_BUFFER_SIZE 21
 
+#ifndef ENUM_S_COMMANDS
+#define ENUM_S_COMMANDS
+typedef enum s_commands {
+  ADD, RM, LS, SYNC, QUIT, NA 
+} commands;
+#endif
+
 #ifndef STRUCT_BLOCKCHAIN_ARGS
 #define STRUCT_BLOCKCHAIN_ARGS
 typedef struct s_arguments {
@@ -32,3 +39,35 @@ int argumentsInitialize(ArgumentsPtr args);
 
 //Return pointer to read_buffer
 char *getReadBuffer(ArgumentsPtr args);
+
+//Return enum command 
+commands find_command(char *arg);
+
+//Check if input string is a positive integer number
+int isPosNum(char *str);
+
+//Return string_array that is a subarray of the input string array
+string_array *subarray(char **argv, int start_index, int end_index);
+
+int isAddBlock(string_array *args, commands command);
+
+int isAddNode(string_array *args, commands command);
+
+int isRmBlock(string_array *args, commands command);
+
+int isRmNode(string_array *args, commands command);
+
+//Check subsequent arguments for correct arguments following "block"
+int parse_block_args(char **block_args, commands command);
+
+//Check subsequent arguments for correct arguments following "node"
+int parse_node_args(char *node_args, commands command);
+
+//if command = ADD or RM, check subsequent arguments for correct arguments
+int parse_add_rm_args(string_array *args, commands command);
+
+//If command = LS, check subsequent arguments for no arguments or -l option
+int parse_ls_args(string_array *args);
+
+//Parse arguments read from stdin
+int parse_arguments(char *args_array);
