965a64d2d1e934a400ff493c74f4e9224904646b
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-10 23:58:05 -0800

    Move Node code to node.c .h

---
+++ node.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "node.h"
+
+int getNid(NodePtr this) {
+  return this->nid;
+}
+
+char *getBid(NodePtr this) {
+  return this->bid;
+}
+
+void setNid(NodePtr this, int nid) {
+  this->nid = nid;
+}
+
+void setBid(NodePtr this, char *bid) {
+  this->bid = bid;
+}
+
+//Allocate memory for Node struct
+NodePtr nodeConstructor(void) {
+  NodePtr node = malloc(sizeof(Node));
+  nodeInitialize(node);
+  return node;
+}
+
+//Free allocated memory for Node struct
+int nodeDestructor(NodePtr node) {
+  free(node);
+  return 0;
+}
+
+//Initialize Node members
+int nodeInitialize(NodePtr node) {
+  node->nid = 0;
+  node->bid = "";
+  node->next_node = NULL;
+  node->prev_node = NULL;
+  node->getNid = getNid;
+  node->getBid = getBid;
+  node->setNid = setNid;
+  node->setBid = setBid;
+  return 0;
+}
