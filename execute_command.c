970ed4d5e39e1450cc7d16a813ec96ad5fc60495
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-21 22:18:05 -0800

    Start execute_command function

---
+++ execute_command.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "blockchain.h"
+#include "helpers.h"
+
+void execute_command(BlockchainPtr blockchain, string_array *split_read_buffer) {
+
+}
