a7679231b9b66a7369596262c32845a3465604a0
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-22 15:01:49 -0800

    Complete nid function
    Resolved unused parameters

--- execute_command.c
+++ execute_command.c
@@ -21,12 +21,21 @@ int is_node(char *component) {
   return strcmp(component, "node");
 }
 
-void nid(BlockchainPtr blockchain, char *prev_cmd, char *component, char *nid) {
+void nid(BlockchainPtr blockchain, char *prev_cmd, char *component, char *bid, char *nid) {
   printf("nid function reached\n");
   int int_nid = atoi(nid); 
   
   if (is_add(prev_cmd) == 0 && is_node(component) == 0) {
     blockchain->addNode(blockchain, &(blockchain->blockchain_head), int_nid);
+  } else if (is_add(prev_cmd) == 0 && is_block(component) == 0) {
+    printf("add block function reached\n");
+    blockchain->addBlock(blockchain, &(blockchain->blockchain_head), bid, int_nid);
+  } else if (is_remove(prev_cmd) == 0 && is_node(component) == 0) {
+    blockchain->removeNode(blockchain, &(blockchain->blockchain_head), int_nid);
+  } else if (is_remove(prev_cmd) == 0 && is_block(component) == 0) {
+    blockchain->removeBlock(blockchain, &(blockchain->blockchain_head), bid, int_nid);
+  } else {
+    printf(NO_COMMAND_FOUND);
   }
 }
 
@@ -35,11 +44,10 @@ void bid(BlockchainPtr blockchain, string_array *split_read_buffer, char *prev_c
   void (*args)(BlockchainPtr, char *, char *, char *, char *) = nid;
   char *subsequent_arg = split_read_buffer->array[4];
   char *char_nid = split_read_buffer->array[3];
-  char *char_bid = split_read_buffer->array[2];
 
   //Check for extra arguments after the expected last argument
   if (subsequent_arg == NULL) {
-    (*args)(blockchain, prev_cmd, "block", char_bid, char_nid);
+    (*args)(blockchain, prev_cmd, component, bid, char_nid);
   } else {
     printf(NO_COMMAND_FOUND);
   }
@@ -53,7 +61,7 @@ void block(BlockchainPtr blockchain, string_array *split_read_buffer, char *prev
   char *char_bid = split_read_buffer->array[2];
 
   if (subsequent_arg == NULL && char_nid != NULL) {
-    (*args)(blockchain, split_read_buffer, prev_cmd, "bid", char_bid);
+    (*args)(blockchain, split_read_buffer, prev_cmd, "block", char_bid);
   } else {
     printf(NO_COMMAND_FOUND);
   }
@@ -114,10 +122,11 @@ void remove_(BlockchainPtr blockchain, string_array *split_read_buffer) {
 void list(BlockchainPtr blockchain, string_array *split_read_buffer) {
   char *lflag = split_read_buffer->array[1];
   char *subsequent_args = split_read_buffer->array[2];
-
+  printf("List function entered\n");
   if (split_read_buffer->array[2] != NULL) {
     printf(NO_COMMAND_FOUND);
   } else if (lflag != NULL && strncmp(lflag, "-l", 2) == 0 && subsequent_args == NULL) {
+    printf("else if statement entered\n");
     blockchain->ls(&(blockchain->blockchain_head), PRINT_BID);
   } else if (lflag == NULL) {
     blockchain->ls(&(blockchain->blockchain_head), NO_BID);
@@ -138,7 +147,7 @@ void execute_command(BlockchainPtr blockchain, string_array *split_read_buffer) 
   char *command_strings[] = {"add", "rm", "ls", "sync", "quit"};
   char *command_arg = split_read_buffer->array[0];
   int i = 0;
-
+  printf("Start execution_command\n");
   while (i < 5) {
     if (strcmp(command_strings[i], command_arg) == 0) {
       (*command[i])(blockchain, split_read_buffer);
