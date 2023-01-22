518a5df1c573f04fc488e654f31a2d67490b622c
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-22 00:52:56 -0800

    execute add and remove

--- execute_command.c
+++ execute_command.c
@@ -5,20 +5,50 @@
 #include "helpers.h"
 #include "status.h"
 
-void block(BlockchainPtr blockchain, string_array *split_read_buffer) {
-  
+void block(BlockchainPtr blockchain, string_array *split_read_buffer, char *arg) {
+  printf("block function reached\n");
 }
 
-void node(BlockchainPtr blockchain, string_array *split_read_buffer) {
-
+void node(BlockchainPtr blockchain, string_array *split_read_buffer, char *arg) {
+  printf("node function reached\n");
 }
 
 void add(BlockchainPtr blockchain, string_array *split_read_buffer) {
   printf("add\n");
+  void (*args[])(BlockchainPtr, string_array *, char *) = {block, node};
+  char *arg_strings[] = {"block", "node"};
+  char *add_arg = split_read_buffer->array[1];
+  int i = 0;
+
+  while (i < 2) {
+    if (add_arg != NULL && strcmp(arg_strings[i], add_arg) == 0) {
+      (*args[i])(blockchain, split_read_buffer, "add"); 
+      break;
+    }
+    i++;
+  }
+  if (i == 2) {
+    printf(NO_COMMAND_FOUND);
+  }
 }
 
 void remove_(BlockchainPtr blockchain, string_array *split_read_buffer) {
   printf("remove\n");
+  void (*args[])(BlockchainPtr, string_array *, char *) = {block, node};
+  char *arg_strings[] = {"block", "node"};
+  char *add_arg = split_read_buffer->array[1];
+  int i = 0;
+
+  while (i < 2) {
+    if (add_arg != NULL && strcmp(arg_strings[i], add_arg) == 0) {
+      (*args[i])(blockchain, split_read_buffer, "remove"); 
+      break;
+    }
+    i++;
+  }
+  if (i == 2) {
+    printf(NO_COMMAND_FOUND);
+  }
 }
 
 void list(BlockchainPtr blockchain, string_array *split_read_buffer) {
@@ -46,10 +76,11 @@ void execute_command(BlockchainPtr blockchain, string_array *split_read_buffer) 
   void (*command[])(BlockchainPtr, string_array *) 
     = {add, remove_, list, sync, quit};
   char *command_strings[] = {"add", "rm", "ls", "sync", "quit"};
+  char *command_arg = split_read_buffer->array[0];
   int i = 0;
 
   while (i < 5) {
-    if (strcmp(command_strings[i], split_read_buffer->array[0]) == 0) {
+    if (strcmp(command_strings[i], command_arg) == 0) {
       (*command[i])(blockchain, split_read_buffer);
       break;
     }
