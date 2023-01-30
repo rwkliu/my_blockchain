e2ab93ae452cd5e8a6a6c3d0c8912310cd8acf9d
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-29 16:51:38 -0800

    Check null read_buffer before split_read_buffer

--- my_blockchain.c
+++ my_blockchain.c
@@ -21,8 +21,14 @@ int main() {
   while ((read_str = my_readline(0)) != NULL) {
     print_prompt(blockchain.num_nodes, blockchain.sync_state);
     strncpy(args.read_buffer, read_str, strlen(read_str));
+    printf("Before printing args buffer\n");
+    printf("args buffer: %s\n", args.read_buffer);
+    if (args.read_buffer[0] == '\0') {
+      break;
+    }
     args.split_read_buffer = args.splitInput(args.read_buffer, " ");
     execute_command(&blockchain, args.split_read_buffer);
+    args.clearBuffers(&args);
   }
 
   free_blockchain(&blockchain);
