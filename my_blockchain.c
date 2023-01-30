b314aed81da782ab53183fc7586e347e9ba0cb95
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-30 14:43:12 -0800

    Free read_str after copying to read_buffer

--- my_blockchain.c
+++ my_blockchain.c
@@ -22,15 +22,14 @@ int main() {
   
   while ((read_str = my_readline(0)) != NULL) {
     strncpy(args.read_buffer, read_str, strlen(read_str));
+    free(read_str);
     if (args.read_buffer[0] == '\0') {
-      free(read_str);
       continue;
     }
     args.split_read_buffer = args.splitInput(args.read_buffer, " ");
     execute_command(&blockchain, args.split_read_buffer);
     args.clearBuffers(&args);
     print_prompt(blockchain.num_nodes, blockchain.sync_state);
-    free(read_str);
   }
 
   free_blockchain(&blockchain);
