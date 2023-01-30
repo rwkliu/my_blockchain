22948528468522599962d25d17db74ab3c171a02
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-01-29 16:44:55 -0800

    Apply 1st iteration of debug_readline

--- my_blockchain.c
+++ my_blockchain.c
@@ -16,18 +16,11 @@ int main() {
   blockchainConstructor(&blockchain);
   Arguments args;
   argumentsConstructor(&args);
+  char *read_str = NULL;
 
-  while (is_not_quit(&args)) {
-    args.clearBuffers(&args);
+  while ((read_str = my_readline(0)) != NULL) {
     print_prompt(blockchain.num_nodes, blockchain.sync_state);
-    char *readline = my_readline(0);
-    if (readline == NULL || readline[0] == '\0') {
-      free(readline);
-      break;
-    }
-    strncpy(args.read_buffer, readline, 20);
-    free(readline);
-    // printf("%s\n", args.read_buffer);
+    strncpy(args.read_buffer, read_str, strlen(read_str));
     args.split_read_buffer = args.splitInput(args.read_buffer, " ");
     execute_command(&blockchain, args.split_read_buffer);
   }
