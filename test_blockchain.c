f77e7ac89b3f9a588cede8d94046fb755b78cdf9
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-02-04 21:54:16 -0800

    Synchronize first node
    Works but has memory leaks

--- test_blockchain.c
+++ test_blockchain.c
@@ -7,23 +7,36 @@
 #include "helpers.h"
 #include "arguments_blockchain.h"
 
-int block_found(Block *blockref, char *bid) {
-  while (blockref) {
-    if (strncmp(blockref->bid, bid, strlen(bid)) == 0) {
+int block_found(Block **blockref, char *bid) {
+  while (*blockref) {
+    if (strncmp((*blockref)->bid, bid, strlen(bid)) == 0) {
       return 1;
     }
-    blockref = blockref->next_block;
+    blockref = &(*blockref)->next_block;
   }
   return 0;
 }
 
 void synchronize_nodes(BlockchainPtr blockchain, Node **noderef) {
- if (blockchain->sync_state == NOT_SYNCED) {
-   if (block_found((*noderef)->bid_head, "21")) {
-     printf("block found\n");
-   }
- }
- printf("OK\n");
+  if (blockchain->sync_state == NOT_SYNCED) {
+    Node **node_tracer = noderef;
+    Node **node_to_check = &(*noderef)->next_node;
+    Block **block_tracer = &(*node_to_check)->bid_head;
+
+    //Synchronize first node
+    while (*node_to_check) {
+      while (*block_tracer) {
+        if (!block_found(&(*node_tracer)->bid_head, (*block_tracer)->bid)) {
+          blockchain->addBlock(blockchain, node_tracer, (*block_tracer)->bid, (*node_tracer)->nid);
+        }
+        block_tracer = &(*block_tracer)->next_block;
+      }
+      node_to_check = &(*node_to_check)->next_node;
+      block_tracer = &(*node_to_check)->bid_head;
+    }
+
+  }
+  printf("OK\n");
 }
 
 int main() {
@@ -46,6 +59,7 @@ int main() {
   //Print the blockchain
   blockchain.ls(&(blockchain.blockchain_head), PRINT_BID);
   synchronize_nodes(&blockchain, &(blockchain.blockchain_head));
+  blockchain.ls(&(blockchain.blockchain_head), PRINT_BID);
   
   free_blockchain(&blockchain);
 
